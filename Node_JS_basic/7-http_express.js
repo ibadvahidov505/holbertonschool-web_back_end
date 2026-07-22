1316c5b4a9372690c4d2bfbe9633f0c1c96d1eb6
ibadvahidov505 <ibadvahidov505@gmail.com>
2026-07-23 01:49:47 +0400

    Update 7-http_express.js

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,34 +1,26 @@
 const express = require('express');
 const countStudents = require('./3-read_file_async');
 
-function createApp(database) {
-  const app = express();
-
-  app.get('/', (req, res) => {
-    res.type('text');
-    res.send('Hello Holberton School!');
-  });
-
-  app.get('/students', (req, res) => {
-    countStudents(database)
-      .then((data) => {
-        res.type('text');
-        res.status(200).send(`This is the list of our students\n${data}`);
-      })
-      .catch((err) => {
-        res.type('text');
-        res.status(500).send(`This is the list of our students\n${err.message}`);
-      });
-  });
-
-  return app;
-}
-
+const app = express();
 const database = process.argv[2];
-const app = createApp(database);
 
-if (require.main === module) {
-  app.listen(1245);
-}
+app.get('/', (req, res) => {
+  res.type('text');
+  res.send('Hello Holberton School!');
+});
 
-module.exports = createApp;
+app.get('/students', (req, res) => {
+  res.type('text');
+
+  countStudents(database)
+    .then((data) => {
+      res.send(`This is the list of our students\n${data}`);
+    })
+    .catch((err) => {
+      res.send(`This is the list of our students\n${err.message}`);
+    });
+});
+
+app.listen(1245);
+
+module.exports = app;
