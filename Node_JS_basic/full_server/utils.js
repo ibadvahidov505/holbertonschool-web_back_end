d88507b2c3fe845917cc3c0a9f2066aec09633b1
ibadvahidov505 <ibadvahidov505@gmail.com>
2026-07-23 01:58:09 +0400

    Create utils.js

---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,29 @@
+import fs from 'fs';
+
+const readDatabase = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+
+    const lines = data.trim().split('\n');
+    const students = lines.slice(1);
+
+    const fields = {};
+
+    students.forEach((line) => {
+      const [firstname, , , field] = line.split(',');
+
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+
+      fields[field].push(firstname);
+    });
+
+    resolve(fields);
+  });
+});
+
+export default readDatabase;
