3d7cba10f41e9d7b544f1b90a1aacb61cfdc7f4c
ibadvahidov505 <ibadvahidov505@gmail.com>
2026-07-23 01:47:41 +0400

    Update 7-http_express.js

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,29 +1,34 @@
 const express = require('express');
 const countStudents = require('./3-read_file_async');
 
-const app = express();
+function createApp(database) {
+  const app = express();
+
+  app.get('/', (req, res) => {
+    res.type('text');
+    res.send('Hello Holberton School!');
+  });
+
+  app.get('/students', (req, res) => {
+    countStudents(database)
+      .then((data) => {
+        res.type('text');
+        res.status(200).send(`This is the list of our students\n${data}`);
+      })
+      .catch((err) => {
+        res.type('text');
+        res.status(500).send(`This is the list of our students\n${err.message}`);
+      });
+  });
+
+  return app;
+}
+
 const database = process.argv[2];
+const app = createApp(database);
 
-app.get('/', (req, res) => {
-  res.type('text');
-  res.send('Hello Holberton School!');
-});
-
-app.get('/students', (req, res) => {
-  countStudents(database)
-    .then((data) => {
-      res.type('text');
-      res.send(`This is the list of our students\n${data}`);
-    })
-    .catch((err) => {
-      res.status(500).type('text');
-      res.send(`This is the list of our students\n${err.message}`);
-    });
-});
-
-// Yalnız birbaşa işə salınanda serveri başlat
 if (require.main === module) {
   app.listen(1245);
 }
 
-module.exports = app;
+module.exports = createApp;
