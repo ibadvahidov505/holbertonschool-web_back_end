834fac90428041697d673fe16f27eaa6e5ee566e
ibadvahidov505 <ibadvahidov505@gmail.com>
2026-07-23 01:59:57 +0400

    Create StudentsController.js

---
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,43 @@
+import readDatabase from '../utils';
+
+class StudentsController {
+  static getAllStudents(req, res) {
+    const database = process.argv[2];
+
+    readDatabase(database)
+      .then((fields) => {
+        let response = 'This is the list of our students';
+
+        Object.keys(fields)
+          .sort((a, b) => a.localeCompare(b))
+          .forEach((field) => {
+            response += `\nNumber of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`;
+          });
+
+        res.status(200).send(response);
+      })
+      .catch(() => {
+        res.status(500).send('Cannot load the database');
+      });
+  }
+
+  static getAllStudentsByMajor(req, res) {
+    const database = process.argv[2];
+    const { major } = req.params;
+
+    if (major !== 'CS' && major !== 'SWE') {
+      res.status(500).send('Major parameter must be CS or SWE');
+      return;
+    }
+
+    readDatabase(database)
+      .then((fields) => {
+        res.status(200).send(`List: ${fields[major].join(', ')}`);
+      })
+      .catch(() => {
+        res.status(500).send('Cannot load the database');
+      });
+  }
+}
+
+export default StudentsController;
